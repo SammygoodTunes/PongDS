6595a4d4e85242d1bb493eb2b9592828a17d41e2
SammygoodTunes <56520787+SammygoodTunes@users.noreply.github.com>
2023-09-05 22:31:53 +0200

    Update main.c

--- source/main.c
+++ source/main.c
@@ -126,7 +126,8 @@ int main(void) {
 	mm_sound_effect win = {{ SFX_WIN }, (int)(1.0f * (1<<10)), 0, 255, 127,};
 	mm_sound_effect lose = {{ SFX_LOSE }, (int)(1.0f * (1<<10)), 0, 255, 127,};
 
-	mmStart(MOD_PONGDS, MM_PLAY_LOOP);
+	// Uncomment to enable terrible music
+	// mmStart(MOD_PONGDS, MM_PLAY_LOOP);
 
 	// mm_sfxhand sfxHander = 0;
 
